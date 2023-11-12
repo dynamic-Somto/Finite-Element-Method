c718acf0f3656622456825bbd42bfecf81eb740f
mide <efusanyaae@gmail.com>
2023-11-13 00:52:41 +0100

    updates

--- MEG314/QUESTION_4.py
+++ MEG314/QUESTION_4.py
@@ -1,15 +1,16 @@
-from sympy import symbols, Function, Eq, dsolve
+# import necessary functions from sympy library
+from sympy import symbols, Function, Eq, dsolve, Derivative
 
-# Define the variable and function
+# Define the independent variable and the dependent function
 x = symbols('x')
 y = Function('y')(x)
 
 # Define the differential equation
-diff_eq = Eq(y.diff(x) + y/2, 3/2)
+diff_eq = Eq(Derivative(y, x) + y/2, 3/2)
 
 # Solve the differential equation
 solution = dsolve(diff_eq, y)
 
 # Display the solution
-print("Solution:")
+print("The solution to the differential equation:")
 print(solution)
